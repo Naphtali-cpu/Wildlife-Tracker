6d20f75710324550e9b7aec9b7cc63087cf67463
Naphtali-cpu <naphtali919@gmail.com>
2021-08-17 20:40:01 +0300

    Removed some code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,6 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class App {
     static int getHerokuAssignedPort() {
@@ -17,18 +16,6 @@ public class App {
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
     public static void main(String[] args) {
-
-        Scanner kb = new Scanner(System.in);
-        System.out.println("Enter a number");
-        try {
-            double nb1 = kb.nextDouble();
-            if(nb1<1000)
-                throw new ArithmeticException();
-            else System.out.println( "result : " + Math.sqrt(nb1) );
-        } catch (ArithmeticException e) {
-            System.out.println("Ha!! This was an exception, Welcome to wildlife tracker.");
-        }
-
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
